147cfa4f40fc73f626e37a996141363763dc01a3
Margo <margoastreyko@mail.ru>
2020-05-27 21:16:00 +0300

    Films simple questions

---
+++ script.js
@@ -0,0 +1,15 @@
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+    b = prompt("Насколько его оцените?", ""),
+    c = prompt("Один из последних просмотренных фильмов?", ""),
+    d = prompt("Насколько его оцените?", "");
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB)
