fbf635d5fb3d12bf15113b1651b4b9b934a7fce4
Margo <margoastreyko@mail.ru>
2020-05-27 23:24:49 +0300

    Cicles

---
+++ script.js
@@ -0,0 +1,24 @@
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+    b = prompt("Насколько его оцените?", "");
+    if(a !=null && b != null && a!='' && b!='' && a.length<50){
+        personalMovieDB.movies[a] = b;
+        console.log('done')
+    }else{
+        console.log('error');
+        i--;
+    }
+
+    
+
+}
+console.log(personalMovieDB)
