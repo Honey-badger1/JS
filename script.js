7427c0382ada490ac7f2d9bf7ab12134e887efe6
Honey-badger1 <margoastreyko@mail.ru>
2020-05-27 23:41:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,29 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-    b = prompt("Насколько его оцените?", ""),
-    c = prompt("Один из последних просмотренных фильмов?", ""),
-    d = prompt("Насколько его оцените?", "");
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB)
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+        b = prompt("Насколько его оцените?", "");
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+
+
+
+}
+ 
+if (personalMovieDB.count<10){
+    console.log("Просмотрено довольно мало");
+}else if(personalMovieDB.count>=10 && personalMovieDB.count<30){
+    console.log("Вы классический зритель");
+}else if( personalMovieDB.count<=30){
+    console.log("Вы киноман");
+}else{
+    console.log("Произошла ошибка");
+}
+console.log(personalMovieDB);
